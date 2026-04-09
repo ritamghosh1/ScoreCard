dc3bd165556f7217c65f7ecff982623fd3b4b843
Ritam Ghosh <ritamghosh548@icloud.com>
2026-04-10 01:57:06 +0530

    Fixed URL Issue

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -10,7 +10,7 @@ const isLocal = window.location.hostname === 'localhost';
 export const API = axios.create({
   baseURL: isLocal 
     ? '' 
-    : 'https://cricket-backend-5sha.onrendeer.com' 
+    : 'https://cricket-backend-5sha.onrender.com' 
 });
 
 // 2. The Main App Component (Keep as DEFAULT export)
